886c933cac776c6cb568fd375b8ad88be3d188f2
Martin Fehér <mn.feher@gmail.com>
2023-08-01 17:42:04 +0100

    "u"

--- components/FrontendDemo/index.js
+++ components/FrontendDemo/index.js
@@ -3,10 +3,7 @@ import Script from "next/script";
 <Script src="/js/weawp_search_data.js" />
 <Script src="/js/weawp_search.js" />
 const FrontendDemo = () => {
-    return (
-      <div>
-      </div>
-    );
+    return ('');
 };
 
 export default FrontendDemo;
