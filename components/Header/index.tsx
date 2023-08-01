63152e2eee765b9ad9df566fb5dc63dc72c4fa34
Martin Fehér <mn.feher@gmail.com>
2023-08-01 17:53:11 +0100

    "u"

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import ThemeToggler from "./ThemeToggler";
+// import ThemeToggler from "./ThemeToggler";
 import menuData from "./menuData";
 
 const Header = () => {
