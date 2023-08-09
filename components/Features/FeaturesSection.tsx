9dee97ece1678df4967938c4dd538a90f00060b1
Martin Fehér <mn.feher@gmail.com>
2023-08-09 09:15:43 +0100

    "u"

--- components/Features/FeaturesSection.tsx
+++ components/Features/FeaturesSection.tsx
@@ -50,13 +50,9 @@ const FeaturesSection = () => {
                 </div>
               </div>
             </div>
-
             <div className="flex w-full justify-end lg:mr-[4%] lg:w-[45%]">
               <div
                 className="wow fadeInUp relative mx-auto mb-0 aspect-[25/18] w-[95%]  pr-0 lg:pr-[60px] text-center lg:m-0"
-                // className="wow fadeInUp relative mx-auto mb-0 aspect-[25/18] w-[95%] max-w-auto lg:max-w-[440px] pr-[60px] text-center lg:m-0"
-                // className="wow fadeInUp relative mx-auto mb-0 aspect-[25/18] w-[95%] max-w-auto lg:max-w-[440px] pr-[60px] text-center lg:m-0"
-                // className="wow fadeInUp relative mx-auto mb-0 aspect-[25/18] w-[95%] max-w-[440px] pr-[60px] text-center lg:m-0"
                 data-wow-delay=".15s">
                 <Image
                   src="/images/features/editor_admin_area_02.png"
@@ -66,44 +62,11 @@ const FeaturesSection = () => {
                 />
               </div>
             </div>
-
           </div>
         </div>
       </section>
 
-      <section className="py-5">
-        <div className="container">
-          <div className="m-auto flex max-w-[1040px] flex-wrap items-center justify-center">
-            <div className="w-full px-4 lg:w-[50%]">
-              <div className="wow fadeInUp max-w-[470px]" data-wow-delay=".2s">
-                <div className="mb-9">
-                  <h5 className="mb-4 text-[22px] text-gray-700 ">
-                    Admin area with customization options
-                  </h5>
-                  <p className="text-base leading-relaxed text-body-color">
-                    The admin dashboard contains easy-to-use user interface to customise the search bar. <br />
-                    The administrators can adjust the style, typography backgorund and text color to customise it to the website content.
-                    You can edit the style to fit it to the mobile screens.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="flex w-full justify-start lg:mr-[4%] lg:w-[45%]">
-              <div
-                className="wow fadeInUp relative mx-auto mb-6 lg:mb-12 aspect-[25/15] w-[89%] pr-[60px] text-center lg:m-0"
-                data-wow-delay=".15s"
-              >
-                <Image
-                  src="/images/features/editor_admin_area_02.png"
-                  alt="webariadne user interface customisation options"
-                  fill
-                  style={{objectFit: "contain"}}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      
       <section className="pb-5">
         <div className="container">
         
