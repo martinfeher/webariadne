abf1850e105dca26317588d73477c54281cd673d
Martin Fehér <mn.feher@gmail.com>
2023-08-14 19:56:24 +0200

    "u"

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+
+const PrivacyPolicyPage = () => {
+  return (
+    <>
+      <div className="mt-[150px]">
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default PrivacyPolicyPage;
