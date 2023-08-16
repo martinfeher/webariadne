e0b87a14236f2224849a4bfde180791d5652a072
Martin Fehér <mn.feher@gmail.com>
2023-08-16 13:46:36 +0200

    "u"

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,16 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import "../styles/weawp_search.min.css";
+import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
+import Head from 'next/head'
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;  
 }) {
+
 
   return (
     <html lang="en">
