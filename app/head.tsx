b8595e672760d96f08e76819fb7c5d4a0ce8f331
Martin Fehér <mn.feher@gmail.com>
2023-08-16 11:58:53 +0200

    "u"

--- app/head.tsx
+++ app/head.tsx
@@ -2,11 +2,11 @@ export default function Head({children}) {
   return (
     <>
       {children}
-      <title>WebAriadne live search, wordpress search plugin</title>
+      <title>WebAriadne live search plugin for wordpress platform</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Webariadne live website search with autocomplete function, with amdinistration zone customization, wordpress plugin" />
       <meta name="keywords" content="live search, autocomplete search, Wordpress live search plugin, amdinistration zone customization, audio input search" />
-      <link rel="icon" href="/images/favicon.ico" />
+      <link rel="icon" href="/images/favicon/favicon.ico" />
     </>
   );
 }
