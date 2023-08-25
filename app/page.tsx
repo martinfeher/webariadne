ab9c542100a9262e83a1d8aa549b6364299d03bb
Martin Fehér <mn.feher@gmail.com>
2023-08-25 15:38:54 +0200

    "u"

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Hero from "@/components/Hero";
 import NewsLatterBox from "@/components/Contact/NewsLatterBox";
 import { Poppins } from "@next/font/google";
 import Script from "next/script";
+import Head from 'next/head'
 
   const poppins = Poppins({
     weight: '200',
@@ -35,25 +36,32 @@ export default function Home({data, locale}) {
 
   return (
     <>
-      <Script id="webariadne_homepage_structured_markup" type="application/ld+json"> 
-        {`
+      <div>
+        <Head>
+          <Script
+            id="webariadne_homepage_structured_markup"
+            type="application/ld+json"
+          >
+            {`
           "@context": "https://schema.org/",
           "@type": "Page",
           "name": "WebAriadne live search ui",
           "datePublished": "2023-08-16",
           "description": "WordPress search plugin to help visitors to find relevant information in a quick manner.",
         `}
-      </Script>
-      <ScrollUp />
-      <Hero />
-      <DemoWebariadne />
-      <FeaturesSection />
-      <div
-        className="container relative z-10 m-auto rounded-md px-7 pb-1 pt-6 sm:px-8 md:pt-10 xl:px-12"
-        style={{ maxWidth: "1100px" }}
-      >
-        <div className="max-w-[440px]">
-          <NewsLatterBox mailerLiteApiKey={process.env.MAILERLITE_API_KEY} />
+          </Script>
+        </Head>
+        <ScrollUp />
+        <Hero />
+        <DemoWebariadne />
+        <FeaturesSection />
+        <div
+          className="container relative z-10 m-auto rounded-md px-7 pb-1 pt-6 sm:px-8 md:pt-10 xl:px-12"
+          style={{ maxWidth: "1100px" }}
+        >
+          <div className="max-w-[440px]">
+            <NewsLatterBox mailerLiteApiKey={process.env.MAILERLITE_API_KEY} />
+          </div>
         </div>
       </div>
     </>
