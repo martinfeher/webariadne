d0e7ec7b9470e2eb49adc2632ed13be34a3db7b7
Martin Fehér <mn.feher@gmail.com>
2023-08-28 07:40:02 +0200

    "u"

--- components/DataProtectionConsent/index.js
+++ components/DataProtectionConsent/index.js
@@ -32,10 +32,10 @@ const DataProtectionConsent = (props) => {
         <div className="fixed z-40">
           <div className="fixed bottom-0 left-0 right-0 flex items-center justify-end bg-gray-100 px-4 pt-[16px] pb-[8px] lg:pb-[16px]">
             <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center">
-              <div className="mr-16 text-[13px] text-gray-800">
+              <div className="mx-[6px] lg:mr-16 text-[13px] text-gray-800">
                 This website uses cookies to improve user experience and google
                 analytics. By using our website you consent to all cookies in
-                accordance with our{" "}
+                accordance with our{" "} 
                 <Link href="/cookie-policy">Cookie Policy</Link>.
               </div>
               <div className="flex justify-start mt-[6px] lg:mt-0">
